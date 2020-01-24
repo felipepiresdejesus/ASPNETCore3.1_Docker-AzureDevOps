0848a5a1a5a09403f117793e11e649a5ae31c2b9
Felipe Jesus <felipepiresdejesus@gmail.com>
2020-01-24 12:26:16 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Live no Canal .NET",
+                    Local = "Felipe Pires de Jesus",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
